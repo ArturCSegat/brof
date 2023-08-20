595b8778fa8b2cee60150f7fb47e3b36f0aabc5f
ArturCSegat <arturcamerasegat@gmail.com>
2023-08-20 15:53:46 -0300

    improved runner.c stability

--- runner.c
+++ runner.c
@@ -3,15 +3,22 @@
 #include <string.h>
 
 int main (int argc, char ** argv) {
-    char args[200];
+    int size = 0;
+
+    for (int i = 0; i < argc; i++) {
+        size += strlen(argv[i]);
+    }
+
+
+    char args[size];
     strcpy(args, " ");
     for (int i = 1; i < argc; i++) {
         strcat(args, argv[i]);
         strcat(args, " ");
 
     }
-    char * run = "python3 main.py ";
-    char command[strlen(run) + strlen(args)];
+    char run[] = "python3 main.py ";
+    char command[strlen(run) + strlen(args) + 10];
     strcat(command, run);
     strcat(command, args);
 
