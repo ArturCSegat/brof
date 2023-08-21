96da14b16a55cf945b38415de5a6a2cc68a69fd0
ArturCSegat <arturcamerasegat@gmail.com>
2023-08-21 09:15:13 -0300

    now using argparse library

--- main.py
+++ main.py
@@ -1,53 +1,40 @@
-import sys
+import argparse
 import command_funcs
 
+parser = argparse.ArgumentParser(description="CLI tool for keeping changes to a file updated in different locations")
+# parser.add_argument("action", type=str, choices=["add", "refresh", "dir", "show", "clear"], help="File action")
+# parser.add_argument("src", type=str, nargs="?", help="Source path")
+# parser.add_argument("dst", type=str, nargs="?", help="Destination path")
+parser.add_argument("-add", dest="add", nargs=2, help="Add a pair of files")
+parser.add_argument("-refresh", dest="refresh", action="store_true", help="Refresh current pairs to update changes")
+parser.add_argument("-dir", dest="dir", nargs=2, help="Add a pair of directories")
+parser.add_argument("-show", dest="show", action="store_true", help="Show the pairs of the current workspace")
+parser.add_argument("-clear", dest="clear", action="store_true", help="Clear all the pairs from the current workspace")
+
 def main():
-    argc = len(sys.argv)
+    args = parser.parse_args()
 
-    options = []
-    first_file_idx = 1
+    if args.add:
+        command_funcs.add_pair_to_store(args.add[0], args.add[1])   
+    elif args.refresh:
+        to_refresh = command_funcs.find_changed()
+        print("Pairs to be updated:")
+        print(to_refresh)
 
-    # loads options into option list
-    for i in range(1, argc):
-        if sys.argv[i][0] == "-":
-            options.append(sys.argv[i])
-            first_file_idx += 1
-    
-    if len(options):
-        for opt in options:
-            match opt:
-                case "-add":
-                    src = sys.argv[first_file_idx]
-                    dst = sys.argv[first_file_idx + 1]
-                    command_funcs.add_pair_to_store(src, dst)
-                case "-refresh":
-                    to_refresh = command_funcs.find_changed()
-                    print("Pairs to be updated: ")
-                    print(to_refresh)
-                    
-                    for pair in to_refresh:
-                        io = input(f" do you want refresh pair: {pair}? y/n or a for refreshing all pairs")
-                        match io:
-                            case "y":
-                                command_funcs.refresh_pair(pair)
-                            case "n":
-                                continue
-                            case "a":
-                                command_funcs.refresh_pairs(to_refresh)
-                                break
-                    
-                case "-clear":
-                    command_funcs.clear_file_pairs_file()
-                case "-dir":
-                    src = sys.argv[first_file_idx]
-                    dst = sys.argv[first_file_idx + 1]
-                    command_funcs.add_folders(src, dst)
-                case "-show":
-                    command_funcs.show_pairs()
-                case _:
-                    print(f"the mode you entered '{opt}' is not valid, please add a valid mode, -add, -refresh, -clear. -dir, -show")
-    else:
-        print(f"you did not enter a mode, please enter a mode, -add, -refresh, -clear. -dir, -show")
+        for pair in to_refresh:
+            io = input(f"Do you want to refresh pair: {pair}? y/n or a for refreshing all pairs ")
+            if io == "y":
+                command_funcs.refresh_pair(pair)
+            elif io == "a":
+                command_funcs.refresh_pairs(to_refresh)
+                break
+    elif args.dir:
+        command_funcs.add_pair_to_store(args.dir[0], args.dir[1])   
+    elif args.show:
+        command_funcs.show_pairs()
+    elif args.clear:
+        command_funcs.clear_file_pairs_file()
 
 if __name__ == "__main__":
     main()
+
